f05f5ab47aed0832e1093fceefcdbc883c108b11
Weiqian-Ni-29 <101934090+Weiqian-Ni-29@users.noreply.github.com>
2022-10-19 17:19:40 +0800

    Update Card.jsx

--- Card.jsx
+++ Card.jsx
@@ -1,14 +1,16 @@
+// A card component with image, title, description and a button.
+
 import React from "react";
 import {MDBCard, MDBCardBody, MDBCardTitle, MDBBtn, MDBCardImage, MDBCardText} from "mdb-react-ui-kit";
 
 function Card(props){
     return (
         <div>
-            <MDBCard className="token img" style={{maxWidth:"22rem"}}>
+            <MDBCard style={{maxWidth:"22rem"}}>
                 <MDBCardImage src={props.dataURL} position="top" style={{marginRight:"4rem", height:"15rem"}}/>
                 <MDBCardBody>
                     <MDBCardTitle>{props.name}</MDBCardTitle>
-                    <MDBCardText>The KryptoBirdz are 20 uniquely generated from the cyberpunk cloud galaxy Msytopia! There is only one of each bird and each bird can only be owned by a single person on the Ethereum blockchain.</MDBCardText>
+                    <MDBCardText>{props.description}</MDBCardText>
                     <MDBBtn href={props.dataURL}>Details</MDBBtn>
                 </MDBCardBody>
             </MDBCard>
